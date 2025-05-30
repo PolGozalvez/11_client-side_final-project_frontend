4e8d8414c731fea482e1488137a05c0eb599f63b
PGJ <pol.gozalvez@mataro.epiaedu.cat>
2025-05-30 11:56:14 +0200

    feat: import api url

--- src/hooks/useBookApi.jsx
+++ src/hooks/useBookApi.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API_URL = "http://localhost:3000/books";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export function useBooksApi() {
     const [books, setBooks] = useState([]);
